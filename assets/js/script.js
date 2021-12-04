22543f3708c00e8d40c8e1a150106ddb528bef19
Ian L <ileach81@gmail.com>
2021-12-04 18:34:43 -0500

    timeblocks set

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,46 @@
-const tasks = {};
+var tasks = {};
+
+var saveTasks = function () {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+var loadTasks = function () {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+
+  // if nothing in localStorage, create a new object to track all task status arrays
+  if (!tasks) {
+    tasks = {
+      tasks: [],
+    };
+  }
+
+  // loop over object properties
+  $.each(tasks, function (list, arr) {
+    // then loop over sub-array
+    arr.forEach(function (task) {
+      createTask(task.text, list);
+    });
+  });
+};
+
+// save button was clicked
+$("#task-form-modal .btn-save").click(function () {
+  // get form values
+  var taskText = $("#modalTaskDescription").val();
+  var taskDate = $("#modalDueDate").val();
+
+  if (taskText && taskDate) {
+    createTask(taskText, taskDate, "toDo");
+
+    // close modal
+    $("#task-form-modal").modal("hide");
+
+    // save in tasks array
+    tasks.toDo.push({
+      text: taskText,
+      date: taskDate,
+    });
+
+    saveTasks();
+  }
+});
