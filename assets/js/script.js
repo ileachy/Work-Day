224b0655ab39f75d0e5401226a5a6d576ef360a2
Ian L <ileach81@gmail.com>
2021-12-05 02:04:34 -0500

    changed date format

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var tasks = {};
 
 var m = moment();
 var today = document.querySelector("#currentDay");
-today.textContent = m.format("LL");
+today.textContent = m.format("llll");
 
 var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
